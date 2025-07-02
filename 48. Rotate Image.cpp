486fe18c283bd8b2a50e3011787f2436e6c3c47b
Ankit Seal <ankitseal2021@gmail.com>
2025-07-03 00:51:48 +0530

    modified:   48. Rotate Image.cpp

--- 48. Rotate Image.cpp
+++ 48. Rotate Image.cpp
@@ -41,22 +41,22 @@ int main()
 {
     Solution s;
     vector<vector<int>> matrix = {{1}};
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     for (int j = 0; j < 3; j++)
-    //     {
-    //         cout << matrix[i][j] << "\t";
-    //     }
-    //     cout << endl;
-    // }
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix.size(); j++)
+        {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
     s.rotate(matrix);
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     for (int j = 0; j < 3; j++)
-    //     {
-    //         cout << matrix[i][j] << "\t";
-    //     }
-    //     cout << endl;
-    // }
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j <  matrix.size(); j++)
+        {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
     return 0;
 }
